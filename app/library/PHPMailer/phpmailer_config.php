962e1b32910507651d38c2b9ffb51f0e8f266735
Viviana Arango <viviana.arango@logisticapp.org>
2019-08-14 14:18:12 -0500

    config php mailer

--- app/library/PHPMailer/phpmailer_config.php
+++ app/library/PHPMailer/phpmailer_config.php
@@ -1,13 +1,26 @@
 <?php
 
-$mail = new PHPMailer();
-$mail->IsSMTP();
-//$mail->SMTPDebug = 2;
-$mail->SMTPAuth = true;
-$mail->SMTPSecure = "ssl";
-$mail->Host = "smtp.gmail.com";
-$mail->Port = 465;
-$mail->Username = "poopsistem2019@gmail.com";
-$mail->Password = "poop123*";
+    $mail = new PHPMailer;
+	$mail->isSMTP();
+	$mail->SMTPDebug = 0;
+	$mail->Debugoutput = 'html';
+	$mail->Host = "smtp.gmail.com";
+	$mail->CharSet = 'UTF-8';
+	$mail->SMTPSecure = 'tls';
+	$mail->Port = 587; // 465 is for ssl
+	$mail->SMTPAuth = true;
+	$mail->Username = "poopsistem2019@gmail.com";
+	$mail->Password = "poop123*";
 
+
+	$mail->IsHTML(true);
+	$mail->Timeout = 10;
+	$mail->SMTPOptions = array(
+            'ssl' => array(
+            'verify_peer' => false,
+            'verify_peer_name' => false,
+            'allow_self_signed' => true
+        )
+    );
+    
 ?>
--- public/push/sendMail.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-sleep(1);
-file_get_contents('http://localhost'.'/api_poop/user/sendEmail/'.$argv[1]);
